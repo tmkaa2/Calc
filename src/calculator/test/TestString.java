f4f56ef0dc79d1caf9c0bd6f7ffc85efedc4298b
tmkaa2 <tvich777@mail.ru>
2013-08-19 20:00:10 +0300

    Added function to check for compliance
    checkStr():
    + checking amount bracers
    + checking expression with grammar
    
    checkStr():
    (How it work?)
    36558->=3
    258->=2
    96->=9
    5->=5

--- src/calculator/test/TestString.java
+++ src/calculator/test/TestString.java
@@ -21,41 +21,92 @@ public class TestString {
    public static  void  main(String[] arg){
        AnalizInputStringToCorectExsspresion analiz = new AnalizInputStringToCorectExsspresion("^(h5h)2");
        AnalizRez rez=analiz.runAnalizExpression();
+
+       //temp Start
        SetGrammar sets=new SetGrammar(new ArrayList<String>(Arrays.asList("+","-","/","*","0","1","2","3","4","5","6",
                                                                           "7","8","9","(",")","min","max","^","sqrt")));
-       String[] rezz= SplitExpressionAtArrayList.ParsStringPart("^(h5h)2");
+       String[] rezz= SplitExpressionAtArrayList.ParsStringPart("(36+(5-(89-9690)))");
        System.out.println("\n----");
-
        for(String sss:rezz){
            System.out.print(sss+" ");
        }
+       System.out.println("\n----");
+       //temp End
 
-       boolean counterErrors=false;
-       for (int i=0;i<rezz.length;i++){
-           counterErrors=false;
-           for (int j=0;j<sets.getSet().size();j++){
-               if(sets.getSet().get(j).equals(rezz[i])== true){
-                   counterErrors=true;
-                   break;
-               }
-           }
-           if(counterErrors == false){
-               System.err.println("\nWe have a problem! Expression is not correct!");
-               System.err.println("\nWith this symbol: {"+rezz[i]+"}");
-               i=rezz.length;
-               break;
-           }
-       }
-
-       System.out.println("\n----\n");
+       //Test
+       checkStr(rezz,sets);
 
 
-       if(rez.error==false && rez.start.length()==0 ){
-           analiz.showListUsedReules();
-       }else{
-           System.err.println("We have problems");
-       }
+
+     //  if(rez.error==false && rez.start.length()==0 ){
+     //      analiz.showListUsedReules();
+     //  }else{
+     //      System.err.println("We have problems");
+     //  }
        
    }
-      
+
+    public static void checkStr(String[] inputExpression,SetGrammar grammarElements){
+        boolean counterErrors=false;
+        String[] modifiedInpExpr=checkStrHelp(inputExpression) ;
+        int counterBreaks=0;
+
+        //Checking brackets at the expression
+        for (int i=0;i<modifiedInpExpr.length;i++){
+            // Amount brackets count up in the expression
+            if(modifiedInpExpr[i].equals("("))
+                counterBreaks++;
+            if(modifiedInpExpr[i].equals(")"))
+                counterBreaks--;
+        }
+
+        for (int i=0;i<modifiedInpExpr.length-1;i++){
+            counterErrors=false;
+
+            //  Checking the expression elements at the include elements of grammar.
+            for (int j=0;j<grammarElements.getSet().size();j++){
+                // Do are brackets enter correct?
+                if(modifiedInpExpr[i].equals(")") && modifiedInpExpr[i+1].equals("(") ||modifiedInpExpr[i].equals("(") && modifiedInpExpr[i+1].equals(")") ){
+                    break;
+                }
+                if(grammarElements.getSet().get(j).equals(modifiedInpExpr[i])== true){
+                    counterErrors=true;
+                }
+            }
+
+            if(counterErrors == false){
+                System.err.println("\nWe have a problem! Expression is not correct!");
+                System.err.println("\nWith this symbol: {"+modifiedInpExpr[i]+"}");
+                i=modifiedInpExpr.length;
+                break;
+            }
+        }
+
+        counterErrors = false;
+        //  Checking the expression elements at the include elements of grammar for last element expression.
+        for (int j=0;j<grammarElements.getSet().size();j++){
+            if(grammarElements.getSet().get(j).equals(inputExpression[inputExpression.length-1])== true){
+                counterErrors=true;
+            }
+        }
+
+
+        if(counterErrors == false){
+            System.err.println("\nWe have a problem! Expression is not correct!\nLast element expression!");
+        }
+
+        if(counterBreaks != 0){
+            System.err.println("\nBrackets are placed incorrectly!\n");
+        }
+
+    }
+
+    public static String[] checkStrHelp(String[] inputExpression){
+        for (int j=0;j<inputExpression.length;j++){
+            if(inputExpression[j].charAt(0)>47 || inputExpression[j].charAt(0)<58){
+                inputExpression[j]=inputExpression[j].charAt(0)+"";
+            }
+        }
+        return inputExpression;
+    }
 }
--- src/kaa/controler/analiz/rez/AnalizRez.java
+++ src/kaa/controler/analiz/rez/AnalizRez.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class AnalizRez {
     public String start;
+    public int indexErrorSimbolAtTheStr;
     public ArrayList<Integer> rules;
     public String[] lexem;
     public boolean error;
